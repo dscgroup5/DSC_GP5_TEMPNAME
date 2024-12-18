9dc923baebb5f1f8e8dd16415f4e981ae13bcd68
Kavya879 <agarwal.kavya1405@gmail.com>
2024-12-18 19:43:22 +0530

    main page changed , removed redundant files

--- DSC2/main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Set the page configuration
-st.set_page_config(page_title="Multi-Page App", page_icon="📑", layout="centered")
-
-# Add a welcome or landing page content
-st.title("Welcome to the Multi-Page App!")
-st.write("Use the sidebar to navigate to different sections.")
